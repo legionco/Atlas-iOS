00fad97bfbdcd60e7ecb3269d121ecde9eddabe2
Kirill Shashkov <shashkov.kirill@gmail.com>
2017-07-06 23:09:47 +0300

    Adjust Address bar margins

--- Code/Views/ATLAddressBarView.m
+++ Code/Views/ATLAddressBarView.m
@@ -31,8 +31,8 @@
 
 @implementation ATLAddressBarView
 
-CGFloat const ATLAddressBarTextViewPadding = 4;
-CGFloat const ATLAddContactButtonRightPadding = -8;
+CGFloat const ATLAddressBarTextViewPadding = 5;
+CGFloat const ATLAddContactButtonRightPadding = -12;
 NSString *const ATLAddContactsButtonAccessibilityLabel = @"Add Contacts Button";
 
 - (id)init
@@ -80,9 +80,9 @@ NSString *const ATLAddContactsButtonAccessibilityLabel = @"Add Contacts Button";
 
 - (void)configureAddressBarTextViewConstrants
 {
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeLeft multiplier:1.0 constant:6]];
     [self addConstraint:[NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeTop multiplier:1.0 constant:ATLAddressBarTextViewPadding]];
-    [self addConstraint:[NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:_addContactsButton attribute:NSLayoutAttributeLeft multiplier:1.0 constant:0]];
+    [self addConstraint:[NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeRight multiplier:1.0 constant:-6]];
     NSLayoutConstraint *bottomConstraint = [NSLayoutConstraint constraintWithItem:_addressBarTextView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-ATLAddressBarTextViewPadding];
     bottomConstraint.priority = 800;
     [self addConstraint:bottomConstraint];
